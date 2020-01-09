5c124ea5e79ecf7b990882de0d3cea6812e17e59
Bruce Oh <bruceoh@winsure.co.kr>
2020-01-09 11:48:48 +0900

    working on home screen

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,15 +1,115 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomeSceen extends StatelessWidget {
   const HomeSceen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    ScreenUtil.init(context);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Title'),
+        backgroundColor: Colors.white,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Image.asset(
+              "assets/images/imgLogo.png",
+              height: 24,
+            ),
+            Icon(
+              Icons.menu,
+              color: Colors.grey[400],
+              size: 30,
+            )
+          ],
+        ),
       ),
-      body: Container(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.home),
+            title: new Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.mail),
+            title: new Text('Messages'),
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person), title: Text('Profile'))
+        ],
+      ),
+      body: Container(
+        width: ScreenUtil().setWidth(ScreenUtil.screenWidth),
+        height: ScreenUtil().setHeight(560),
+        color: Colors.greenAccent,
+        child: Padding(
+          padding: const EdgeInsets.only(right: 16, left: 16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                child: Text(
+                  "윈슈어 고객팀",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 22),
+                ),
+              ),
+              Text(
+                "어떤 보험이 필요하세요?",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 22),
+              ),
+              SizedBox(
+                height: ScreenUtil().setHeight(50),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Container(
+                    height: ScreenUtil().setHeight(100),
+                    child: RaisedButton(
+                      color: Colors.red,
+                      onPressed: () {},
+                      child: Text(
+                        "내게 맞는 보험 찾기",
+                        style: TextStyle(fontSize: 16, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    height: ScreenUtil().setHeight(100),
+                    child: RaisedButton(
+                      color: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadiusDirectional.circular(0),
+                        side: BorderSide(color: Colors.red),
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        "설계사 찾기",
+                        style: TextStyle(fontSize: 16, color: Colors.red),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,7 +9,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:winsure_demo_git/src/root.dart';
 
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
